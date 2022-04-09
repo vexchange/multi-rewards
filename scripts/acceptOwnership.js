2e6f051ae60ecb9f82ac312f195bdbbe672044f5
Kenneth Ashley <ken.u.diggitt@gmail.com>
2022-04-09 11:50:13 -0400

    Add acceptOwnership and nominateNewOwner scripts

---
+++ scripts/acceptOwnership.js
@@ -0,0 +1,45 @@
+// ES5 style
+const ConnexDriver = require('@vechain/connex-driver');
+const Framework = require("@vechain/connex-framework").Framework;
+const find = require('lodash/find');
+
+const config = require('./deploymentConfig');
+const Multirewards = require('../build/contracts/MultiRewards.json');
+
+const { Driver, SimpleNet, SimpleWallet } = ConnexDriver;
+
+const [network, multiRewards] = process.argv.slice(2)
+
+// ensure we have appropriate arguments
+if (!network || !multiRewards) {
+  console.error("Usage: node scripts/acceptOwnership [mainnet|testnet] [Multirewards address]");
+
+  process.exit();
+}
+
+(async() => {
+  const wallet = new SimpleWallet();
+
+  wallet.import(config.privateKey);
+
+  console.log("Using wallet address:", wallet.keys[0].address);
+
+  const net = new SimpleNet(config.network[network].rpcUrl);
+  const driver = await Driver.connect(net, wallet);
+  const connex = new Framework(driver);
+
+  const acceptOwnershipABI = find(Multirewards.abi, { name: 'acceptOwnership' });
+  const acceptOwnershipMethod = connex.thor.account(multiRewards).method(acceptOwnershipABI);
+
+  const clause = acceptOwnershipMethod.asClause();
+
+  try {
+    const result = await connex.vendor.sign('tx', [{ ...clause }]).request();
+
+    console.log("Result: ", result);
+  } catch(error) {
+    console.error(error);
+  }
+
+  process.exit();
+})();
