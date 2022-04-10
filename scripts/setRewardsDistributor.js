6ef60adc9f018515e82140f2685f292efb1e35eb
A.L <xenide@users.noreply.github.com>
2022-04-10 22:32:11 +0800

    feat: add setRewardsDistributo script

---
+++ scripts/setRewardsDistributor.js
@@ -0,0 +1,47 @@
+// ES5 style
+const ConnexDriver = require('@vechain/connex-driver');
+const Framework = require("@vechain/connex-framework").Framework;
+const find = require('lodash/find');
+
+const config = require('./deploymentConfig');
+const Multirewards = require('../build/contracts/MultiRewards.json');
+
+const { Driver, SimpleNet, SimpleWallet } = ConnexDriver;
+
+const [network, multiRewards, rewardsToken, newRewardsDistributor] = process.argv.slice(2)
+
+// ensure we have appropriate arguments
+if (!network || !multiRewards || !rewardsToken || !newRewardsDistributor) {
+  console.error("Usage: node scripts/setRewardsDuration [mainnet|testnet] [Multirewards address] [Reward token address] [New rewardsDistributor address]");
+
+  process.exit();
+}
+
+(async() => {
+  const wallet = new SimpleWallet();
+
+  wallet.import(config.privateKey);
+
+  console.log("Using wallet address:", wallet.keys[0].address);
+
+  const net = new SimpleNet(config.network[network].rpcUrl);
+  const driver = await Driver.connect(net, wallet);
+  const connex = new Framework(driver);
+
+  const setRewardsDistributorABI = find(Multirewards.abi, { name: 'setRewardsDistributor' });
+  const setRewardsMethod = connex.thor.account(multiRewards).method(setRewardsDistributorABI);
+
+  const clause = setRewardsMethod.asClause(rewardsToken, newRewardsDistributor);
+
+  try {
+    const result = await connex.vendor.sign('tx', [{ ...clause }]).request();
+
+    console.log("Transaction Hash:", result.transactionHash);
+  } catch(error) {
+    console.error(error);
+  }
+
+  process.exit();
+})();
+
+
